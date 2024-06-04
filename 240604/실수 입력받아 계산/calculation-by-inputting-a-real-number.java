9c8ffcd032ee237351e086cd86214ebb06ed5a52
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-06-04 22:46:58 +0900

    Title: 실수 입력받아 계산, Time: 174ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240604/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double result = a + b;
+        System.out.printf("%.2f", result);
+    }
+}
