9c360cf2c07eeca289d45be77f8eae798f77e90f
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-06-09 00:39:42 +0900

    Title: 전화번호 바꾸기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240609/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] arr = s.split("-");
+        System.out.println(arr[0] + "-" + arr[2] + "-" + arr[1]);
+    }
+}
