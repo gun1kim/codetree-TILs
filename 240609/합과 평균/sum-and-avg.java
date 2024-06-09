953f4ea4722b7d1c38b2b8f6991c42d76ec38073
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-06-09 15:05:26 +0900

    Title: 합과 평균, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240609/합과 평균/sum-and-avg.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        double avg = (double) sum / a;
+        System.out.printf("%d %.1f", sum, avg);
+    }
+}
