d44ba980bbda685a2d3f2f01137d0711e177ad20
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-06-17 22:37:49 +0900

    Title: 윤년인가, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240617/윤년인가/is-leap-year.java
+++ 240617/윤년인가/is-leap-year.java
@@ -5,11 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
 
-        if (y % 4 == 0) {
-            if (y % 100 == 0 && y % 400 != 0) {
-                System.out.println("false");
-            }
+        if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0) {
+            System.out.println("true");
         }
-        System.out.println("true");
+        else {
+            System.out.println("false");
+        }
     }
 }
