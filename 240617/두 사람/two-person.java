502ea8f7a8a966560bb02ffa9c334f8fb76394f4
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-06-17 22:11:13 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240617/두 사람/two-person.java
+++ 240617/두 사람/two-person.java
@@ -3,16 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ageA = sc.nextInt();
-        char genA = sc.next().charAt(0);
-        int ageB = sc.nextInt();
-        char genB = sc.next().charAt(0);
+        String personA = sc.nextLine().trim();
+        String personB = sc.nextLine().trim();
 
-        if ((ageA >= 19 || ageB >= 19) && (genA == 'M' || genB == 'M')) {
+        int ageA = Integer.parseInt(personA.substring(0, personA.length() - 1).trim());
+        char genA = personA.charAt(personA.length() - 1);
+        int ageB = Integer.parseInt(personB.substring(0, personB.length() - 1).trim());
+        char genB = personA.charAt(personB.length() - 1);
+        
+
+        if (isOver19AndMale(ageA, genA) || isOver19AndMale(ageB, genB)) {
             System.out.println("1");
         }
         else {
             System.out.println("0");
         }
+    }
+
+    private static boolean isOver19AndMale(int age, char gender) {
+        return age >= 19 && gender == 'M';
     }
 }
