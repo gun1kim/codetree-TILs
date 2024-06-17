6bbf022c4004db19dc64b5b708a7abe11f36acdf
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-06-17 22:00:20 +0900

    Title: 두 사람, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240617/두 사람/two-person.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ageA = sc.nextInt();
+        char genA = sc.next().charAt(0);
+        int ageB = sc.nextInt();
+        char genB = sc.next().charAt(0);
+
+        if (ageA >= 19 || ageB >= 19) {
+            if (genA == 'M' || genB == 'M') {
+                System.out.println("1");
+            }
+        }
+        else {
+            System.out.println("0");
+        }
+    }
+}
