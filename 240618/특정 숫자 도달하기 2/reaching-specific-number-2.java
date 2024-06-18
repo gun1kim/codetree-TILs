234b15b11e979e8775e44fa5d687995b9e400508
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-06-18 23:32:57 +0900

    Title: 특정 숫자 도달하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240618/특정 숫자 도달하기 2/reaching-specific-number-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        for (int i = n; i > 0; i--) {
+            System.out.print(i + " ");
+        }    
+    }
+}
