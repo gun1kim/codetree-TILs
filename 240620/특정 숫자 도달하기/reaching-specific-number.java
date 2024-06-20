32f8ba32d8fb583f750bb09114ec0dc34c766630
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-06-20 23:26:09 +0900

    Title: 특정 숫자 도달하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240620/특정 숫자 도달하기/reaching-specific-number.java
+++ 240620/특정 숫자 도달하기/reaching-specific-number.java
@@ -5,19 +5,31 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
         int sum = 0;
+        int count = 0;
         double avg = 0.0;
+        boolean flag = false;
+
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
-            if (arr[i] < 250) {
-                sum += arr[i];
-                
-            }
-            else {
-                avg = sum / (double) i;
+        }
+
+        for(int i = 0; i < 10; i++) {
+            if (arr[i] >= 250) {
+                flag = true;
                 break;
+            } else {
+                sum += arr[i];
+                count += 1;
             }
         }
 
+        if (!flag) {
+            avg = (double) sum / 10;
+        }
+        else {
+            avg = (double) sum / count;
+        }
         System.out.printf("%d %.1f", sum, avg);
+        
     }
 }
