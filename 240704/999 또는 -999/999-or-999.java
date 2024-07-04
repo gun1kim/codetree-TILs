7cf4dff0512d85f03dbd2cde9d925e481b913986
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-07-04 23:05:06 +0900

    Title: 999 또는 -999, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240704/999 또는 -999/999-or-999.java
+++ 240704/999 또는 -999/999-or-999.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int max = -1000;
-        int min = 1000;
+        int max = -999;
+        int min = 999;
 
         while (true) {
             int n = sc.nextInt();
