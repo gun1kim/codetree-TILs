bd1aea8fc87490ad90616c80b8157cdf0c2257db
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-07-06 23:54:39 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 215ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240706/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int end = n; 
+        while (end > 0) {
+            int maxIndex = 0;
+            for (int i = 0; i < end; i++) {
+                if (arr[i] > arr[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+            System.out.print((maxIndex + 1) + " "); 
+            end = maxIndex;
+        }
+    }
+}
