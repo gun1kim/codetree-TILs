07e6b667a2ae7045fbf9f47b114f1342582b3837
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-07-14 17:26:46 +0900

    Title: 지그재그로 숫자 채우기, Time: 203ms, Memory: 12MB, Status: Passed - CodeTree

--- 240714/지그재그로 숫자 채우기/zigzag-numbering.java
+++ 240714/지그재그로 숫자 채우기/zigzag-numbering.java
@@ -8,18 +8,16 @@ public class Main {
         int num = 0;
         int[][] arr = new int[n][m];
 
-
-        for (int i = 0; i < m; i+=2) {
-            for (int j = 0; j < n; j++) {
-                arr[j][i] = num; 
-                num += 1;
+        for (int i = 0; i < m; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    arr[j][i] = num++;
+                }
             }
-        }    
-
-        for (int i = 1; i < m; i+=2) {
-            for (int j = n-1; j >= 0; j--) {
-                arr[j][i] = num;
-                num += 1;
+            else {
+                for (int j = n-1; j >= 0; j--) {
+                    arr[j][i] = num++;
+                }
             }
         }
 
