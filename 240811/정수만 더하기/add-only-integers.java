1fed30d144478e71387a50c3ea8d8c4926d53cfb
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-08-11 21:20:15 +0900

    Title: 정수만 더하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240811/정수만 더하기/add-only-integers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                result += (int)(s.charAt(i)-'0');
+            }
+        }
+        System.out.print(result);
+    }
+}
