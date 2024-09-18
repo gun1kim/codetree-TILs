a670312574de40a9fd2bc3e0f452ef3cf4493df3
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-09-18 19:38:00 +0900

    Title: 이상한 수열, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240918/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(solution(n));
+    }
+    private static int solution(int n) {
+        if (n == 1) return 1;
+        else if (n == 2) return 2;
+        else {
+            return n/3 + solution(n-1);
+        }
+    }
+}
