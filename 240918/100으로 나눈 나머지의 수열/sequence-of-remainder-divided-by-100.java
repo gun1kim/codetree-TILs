6cd5cb907fc98ae3ba85905422d4961857fd0243
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-09-18 19:06:59 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240918/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(solution(n));
+    }
+
+    private static int solution(int n) {
+        if (n == 1) return 2;
+        else if (n == 2) return 4;
+        else {
+            return solution(n-1) * solution(n-2) % 100;
+        }
+    }
+}
