b7d15f5c0360a34a276a258f3ff3708261474419
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-10-05 17:11:38 +0900

    Title: 원점으로부터의 거리, Time: 252ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241005/원점으로부터의 거리/distance-from-origin.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Point {
+    int x;
+    int y;
+    int idx;
+
+    public Point(int x, int y, int idx) {
+        this.x = x;
+        this.y = y;
+        this.idx = idx;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            points[i] = new Point(sc.nextInt(), sc.nextInt(), i+1);
+        }
+
+        Arrays.sort(points, (a, b) -> {
+            if (Math.abs(a.x + a.y) == Math.abs(b.x + b.y)) {
+                return b.idx - a.idx;
+            }
+            return Math.abs(a.x + a.y) - Math.abs(b.x + b.y);
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(points[i].idx);
+        }
+    }
+}
