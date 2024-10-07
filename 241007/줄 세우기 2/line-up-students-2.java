87db9ca4f88fb97ea57666b668b742bb86adfd2b
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-10-07 22:38:51 +0900

    Title: 줄 세우기 2, Time: 291ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241007/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Student {
+    int h;
+    int w;
+    int idx;
+
+    public Student(int h, int w, int idx) {
+        this.h = h;
+        this.w = w;
+        this.idx = idx;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+        for (int i = 0; i < n; i++) {
+            students[i] = new Student(sc.nextInt(), sc.nextInt(), i+1);
+        }
+
+        Arrays.sort(students, (a, b) -> a.h == b.h ? a.w - b.w : a.h - b.h);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(students[i].h + " " + students[i].w + " " + students[i].idx);
+        }
+    }
+}
