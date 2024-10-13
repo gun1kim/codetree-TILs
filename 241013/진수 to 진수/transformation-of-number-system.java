07916f8f2e2fbea058ec510d04dec7fbb452f507
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-10-13 23:08:16 +0900

    Title: 진수 to 진수, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 241013/진수 to 진수/transformation-of-number-system.java
+++ 241013/진수 to 진수/transformation-of-number-system.java
@@ -10,12 +10,12 @@ public class Main {
         String n = sc.next();
         int num = 0;
         for (int i = 0; i < n.length(); i++) {
-            num = num * i * a + (n.charAt(i) - '0');
+            num = num * a + (n.charAt(i) - '0');
         }
 
         int cnt = 0;
         while (true) {
-            if (num < 2) {
+            if (num < b) {
                 digits[cnt++] = num;
                 break;
             }
