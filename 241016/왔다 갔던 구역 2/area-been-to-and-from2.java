fdbf9dd95d8bf968f0b82d42a27c8802122ff97a
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-10-16 23:02:07 +0900

    Title: 왔다 갔던 구역 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241016/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 241016/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -4,31 +4,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] check = new int[201];
-        int cur = 100;
-        for(int i = 0; i < n; i++) {
+        int[] check = new int[201]; 
+        int cur = 100;  
+
+        for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             char d = sc.next().charAt(0);
 
             if (d == 'L') {
-                for(int j = cur; j > cur - x; j--) {
-                    check[j] ++;
+                for (int j = cur; j > cur - x; j--) { 
+                    check[j]++;
                 }
-                cur -= x;
-            }
-            else if (d == 'R') {
-                for (int j = cur; j < cur + x; j++) {
-                    check[j] ++;
+                cur -= x;  
+            } else if (d == 'R') {
+                for (int j = cur; j < cur + x; j++) { 
+                    check[j]++;
                 }
-                cur += x;
-            }
-        } 
-        int answer = 0; 
-        for (int i = 0; i < 201; i++) {
-            if (check[i] >= 2) {
-                answer +=1;
+                cur += x;  
             }
         }
+
+        int answer = 0;
+        for (int i = 0; i < 201; i++) {
+            if (check[i] >= 2) {
+                answer++;
+            }
+        }
+
         System.out.print(answer);
     }
 }
