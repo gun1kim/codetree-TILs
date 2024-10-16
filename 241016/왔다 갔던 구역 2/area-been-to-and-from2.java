aa0ed9f2def8e0f4d7ed3d0a835507e57886f15a
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-10-16 23:08:00 +0900

    Title: 왔다 갔던 구역 2, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241016/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 241016/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -4,35 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] check = new int[201]; // 위치 범위: -100 ~ 100 (중앙은 100)
-        int cur = 100;  // 시작 위치
-
-        // 명령 수행
-        for (int i = 0; i < n; i++) {
+        int[] check = new int[2001];
+        int cur = 100;
+        for(int i = 0; i < n; i++) {
             int x = sc.nextInt();
             char d = sc.next().charAt(0);
 
             if (d == 'L') {
-                for (int j = cur - 1; j >= cur - x; j--) {
-                    check[j]++;
+                for(int j = cur; j > cur - x; j--) {
+                    check[j] ++;
                 }
-                cur -= x;  // 현재 위치 업데이트
-            } else if (d == 'R') {
-                for (int j = cur + 1; j <= cur + x; j++) {
-                    check[j]++;
-                }
-                cur += x;  // 현재 위치 업데이트
+                cur -= x;
             }
-        }
-
-        // 2번 이상 지나간 구간 계산
-        int answer = 0;
-        for (int i = 0; i < 201; i++) {
+            else if (d == 'R') {
+                for (int j = cur; j < cur + x; j++) {
+                    check[j] ++;
+                }
+                cur += x;
+            }
+        } 
+        int answer = 0; 
+        for (int i = 0; i < 2001; i++) {
             if (check[i] >= 2) {
-                answer++;
+                answer +=1;
             }
         }
-
         System.out.print(answer);
     }
 }
