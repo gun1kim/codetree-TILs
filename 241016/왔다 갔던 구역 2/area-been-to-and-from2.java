9f02f38727a02b549e626efc54f618dc3ad8a275
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-10-16 23:03:15 +0900

    Title: 왔다 갔던 구역 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241016/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 241016/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -4,26 +4,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] check = new int[201]; 
-        int cur = 100;  
+        int[] check = new int[201]; // 위치 범위: -100 ~ 100 (중앙은 100)
+        int cur = 100;  // 시작 위치
 
+        // 명령 수행
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             char d = sc.next().charAt(0);
 
             if (d == 'L') {
-                for (int j = cur; j > cur - x; j--) { 
+                for (int j = cur - 1; j >= cur - x; j--) {
                     check[j]++;
                 }
-                cur -= x;  
+                cur -= x;  // 현재 위치 업데이트
             } else if (d == 'R') {
-                for (int j = cur; j < cur + x; j++) { 
+                for (int j = cur + 1; j <= cur + x; j++) {
                     check[j]++;
                 }
-                cur += x;  
+                cur += x;  // 현재 위치 업데이트
             }
         }
 
+        // 2번 이상 지나간 구간 계산
         int answer = 0;
         for (int i = 0; i < 201; i++) {
             if (check[i] >= 2) {
