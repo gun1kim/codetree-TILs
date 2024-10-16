20f827c4ab24d6858723fd089849f814170ff77d
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-10-16 23:25:12 +0900

    Title: 왔다 갔던 구역 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241016/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 241016/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -6,27 +6,29 @@ public class Main {
         int n = sc.nextInt();
         int[] check = new int[2001];
         int cur = 1000;
-        for(int i = 0; i < n; i++) {
+        
+        for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             char d = sc.next().charAt(0);
 
             if (d == 'L') {
-                for(int j = cur; j > cur - x; j--) {
-                    check[j] ++;
+                for (int j = cur - 1; j >= cur - x; j--) {
+                    check[j] += 1;
                 }
                 cur -= x;
             }
-            else if (d == 'R') {
+            else {
                 for (int j = cur; j < cur + x; j++) {
-                    check[j] ++;
+                    check[j] += 1;
                 }
                 cur += x;
             }
-        } 
-        int answer = 0; 
+        }
+
+        int answer = 0;
         for (int i = 0; i < 2001; i++) {
             if (check[i] >= 2) {
-                answer +=1;
+                answer ++;
             }
         }
         System.out.print(answer);
