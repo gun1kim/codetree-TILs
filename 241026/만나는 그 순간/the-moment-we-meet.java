5825f27aa3e7272764187cb59123c21f3de8a6e8
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-10-26 23:35:26 +0900

    Title: 만나는 그 순간, Time: 139ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241026/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] a = new int[2000001];
+        int[] b = new int[2000001];
+        int cur = 1000000;
+        int timeA = 0;
+        for (int i = 0; i < n; i++) {
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+            if (d == 'L') {
+                for (int j= cur; j > cur - t; j--) {
+                    a[j] = timeA;
+                    timeA ++;
+                }
+                cur -= t;
+            }
+            else if (d == 'R') {
+                for (int j = cur; j < cur + t; j++) {
+                    a[j] = timeA;
+                    timeA ++;
+                }
+            }
+            cur += t;
+        }
+        
+        cur = 1000000;
+        int timeB = 0;
+        for (int i = 0; i < m; i++) {
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+            if (d == 'L') {
+                for (int j = cur; j > cur - t; j--) {
+                    b[j] = timeB;
+                    timeB ++;
+                }
+                cur -= t;
+            }
+            else if (d == 'R') {
+                for (int j = cur; j < cur + t; j++) {
+                    b[j] = timeB;
+                    timeB ++;
+                }
+            }
+            cur += t;
+        }
+        int answer = 0;
+        for (int i = 0; i < 2000001; i++) {
+            if (a[i] == b[i] && a[i] != 0 && b[i] != 0) {
+                answer = a[i];
+                break;
+            }
+        }
+
+        if (answer == 0) answer = -1;
+        System.out.print(answer);
+    }
+}
