9390bd8f604f5ec8e279ee59108fa089c11726c1
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-10-31 23:33:40 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241031/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        int x = 0, y = 0;
+        int dir = 0;
+        String s = sc.next();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'L') {
+                dir = (dir - 1) % 4;
+            }
+            else if (s.charAt(i) == 'R') {
+                dir = (dir + 1) % 4;
+            }
+            else if (s.charAt(i) == 'F') {
+                x += dx[dir];
+                y += dy[dir];
+            }
+        }
+
+        System.out.print(x + " " + y);
+    }
+}
