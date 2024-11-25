eb5070ca42eaf1c83747974b987890c212fe3934
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-11-25 22:37:14 +0900

    Title: 씨 오 더블유 2, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 241125/씨 오 더블유 2/c-o-w-2.java
+++ 241125/씨 오 더블유 2/c-o-w-2.java
@@ -6,22 +6,21 @@ public class Main {
         int n = sc.nextInt();
         String s = sc.next();
         int countC = 0;
-        int countO = 0;
-        int countW = 0;
+        int countCO = 0;
+        int countCOW = 0;
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == 'C') {
                 countC += 1;
             }
             else if (s.charAt(i) == 'O') {
-                countO += 1;
+                countCO += countC;
             }
             else if (s.charAt(i) == 'W') {
-                countW += 1;
+                countCOW += countCO;
             }
         }
 
-        int answer = countC * countO * countW;
-        System.out.print(answer);
+        System.out.print(countCOW);
     }
 }
