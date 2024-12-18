497fcc1ef579c689b435ea9c62b5c0f8495640ca
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-12-18 22:42:23 +0900

    Title: 개발자의 능력 2, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 241218/개발자의 능력 2/ability-of-developer-2.java
+++ 241218/개발자의 능력 2/ability-of-developer-2.java
@@ -1,45 +1,50 @@
 import java.util.*;
 
 public class Main {
-    private static final int n = 6;
-    private static int[] arr = new int[n];
-    private static int answer = Integer.MAX_VALUE;
+    public static final int n = 6;
+    public static int[] arr = new int[6];
+    public static int answer = Integer.MAX_VALUE;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        boolean[] selected = new boolean[n];
 
-        findMinDiff(arr, selected, 0, 0);
-
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                answer = Math.min(answer, getDiff(i, j));
+            }
+        }
         System.out.print(answer);
-
     }
 
-    private static void findMinDiff(int[] arr, boolean[] selected, int start, int depth) {
-        if (depth == 3) {
-            int[] sum = new int[3];
-            int index = 0;
-
-            for (int i = 0; i < arr.length; i++) {
-                if (selected[i]) {
-                    sum[index++] += arr[i];
-                }
-            }
-
-            int max = Math.max(sum[0], Math.max(sum[1], sum[2]));
-            int min = Math.min(sum[0], Math.min(sum[1], sum[2]));
-            answer = Math.min(answer, max - min);
-
-            return;
+    private static int getDiff(int a, int b) {
+        int total = 0;
+        for (int i : arr) {
+            total += i;
         }
-        for (int i = start; i < arr.length; i++) {
-            if (!selected[i]) {
-                selected[i] = true;
-                findMinDiff(arr, selected, i + 1, depth + 1);
-                selected[i] = false;
+
+        int sum1 = arr[a] + arr[b];
+        int sum2 = 0;
+        int sum3 = 0;
+
+        int tempMin = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (i == a || i == b || j == a || j == b) continue;
+
+                sum2 = (arr[i] + arr[j]);
+                sum3 = total - sum1 - sum2;
+
+                int[] arr = {sum1, sum2, sum3};
+                Arrays.sort(arr);
+                int diff = arr[2] - arr[0];
+                tempMin = Math.min(tempMin, diff);
             }
         }
+
+        return tempMin;
     }
 }
