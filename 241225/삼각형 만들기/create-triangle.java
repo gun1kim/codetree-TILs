5ba3c31b2e41fa7d8ead0aa01965c05d9fd85fed
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-12-25 22:24:31 +0900

    Title: 삼각형 만들기, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241225/삼각형 만들기/create-triangle.java
@@ -0,0 +1,40 @@
+import java.util.*; 
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            arr[i][0] = x;
+            arr[i][1] = y;
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (i != j && j != k && i != k) {
+                        int x1 = arr[i][0], y1 = arr[i][1];
+                        int x2 = arr[j][0], y2 = arr[i][1];
+                        int x3 = arr[k][0], y3 = arr[k][1];
+
+                        boolean isXParallel = (y1 == y2 || y2 == y3 || y1 == y3);
+                        boolean isYParallel = (x1 == x2 || x2 == x3 || x1 == x3);
+
+                        if (isXParallel && isYParallel) {
+                            int area = Math.abs(x2 - x1) * (y3 - y1);
+                            answer = Math.max(answer, area);
+                        }
+                    }
+                }
+            }
+        }
+        
+        System.out.print(answer);
+    }
+}
