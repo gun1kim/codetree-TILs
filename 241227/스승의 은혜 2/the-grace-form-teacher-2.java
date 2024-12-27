2b6d9cae9d6ca7e3f9ce39129be80ef0ead7cc4b
gun1kim <67460930+gun1kim@users.noreply.github.com>
2024-12-27 22:38:55 +0900

    Title: 스승의 은혜 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241227/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                int price = (i == j) ? arr[j] / 2 : arr[j];
+                if (sum + price <= b) {
+                    sum += price;
+                    count++;
+                } 
+                else {
+                    break;
+                }
+            }
+            answer = Math.max(answer, count);
+        }
+        System.out.print(answer);
+    }
+}
